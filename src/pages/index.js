5c0de431a77e3e3ce937b58d72de07f0627a1fac
Claus Höfele <claus@claushoefele.com>
2020-07-28 12:03:14 +0200

    Add chakra-ui and react-icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,37 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
+
+import { Box, Text, Flex, Badge, Image } from "@chakra-ui/core"
+import { MdStar } from 'react-icons/md';
+
+const AirBnB = () => (
+  <Box>
+    <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
+    <Flex align="baseline" mt={2}>
+      <Badge colorScheme="pink">Plus</Badge>
+      <Text
+        ml={2}
+        textTransform="uppercase"
+        fontSize="sm"
+        fontWeight="bold"
+        color="pink.800"
+      >
+        Verified &bull; Cape Town
+    </Text>
+    </Flex>
+    <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+      Modern, Chic Penthouse with Mountain, City & Sea Views
+  </Text>
+    <Text mt={2}>$119/night</Text>
+    <Flex mt={2} align="center">
+      <Box as={MdStar} color="orange.400" />
+      <Text ml={1} fontSize="sm"><b>4.84</b> (190)</Text>
+    </Flex>
+  </Box>
+)
 
 const IndexPage = () => (
   <Layout>
@@ -16,6 +45,9 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+
+    <AirBnB />
+
   </Layout>
 )
 
