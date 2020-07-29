6d1bc3842594ed5f9b94faafef95852ea16ed98c
Claus Höfele <claus@claushoefele.com>
2020-07-29 20:16:12 +0200

    List blog entries

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,63 +1,41 @@
 import * as React from "react"
-// import { useStaticQuery, graphql, Link as GatsbyLink } from "gatsby"
+import { useStaticQuery, Link } from "gatsby"
 import {
-  Box,
-  Container,
-  Stack,
-  // Avatar,
-  // chakra,
-  // Badge,
+  VStack,
 } from "@chakra-ui/core"
 import Layout from "../components/layout"
 
 // frontmatter > tags
 function BlogEntries() {
-  // const { allMdx } = useStaticQuery(graphql`
-  //   query AllGuides {
-  //     allMdx(filter: { slug: { glob: "blog/*" } }) {
-  //       nodes {
-  //         excerpt
-  //         frontmatter {
-  //           title
-  //         }
-  //         parent {
-  //           ... on File {
-  //             birthTime
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
+  const { allMdx } = useStaticQuery(graphql`
+    query {
+      allMdx(filter: {fileAbsolutePath: {glob: "**/blog/*"}}) {
+        nodes {
+          fileAbsolutePath
+          slug
+          frontmatter {
+            title
+          }
+        }
+      }
+    }      
+  `)
 
   return (
     <Layout title="Blog" subtitle="A list of blog entries.">
-      <Box>
-        <Container>
-          <Stack spacing="4rem">
-            {/* {allMdx.nodes.map(
-              ({
-                fields: { createdAt, contributors, slug },
-                frontmatter: { title, tags },
-                parent: { birthTime },
-                excerpt,
-              }) => (
-                <GuidePreview
-                  key={title}
-                  url={slug}
-                  title={title}
-                  birthTime={birthTime}
-                  createdAt={createdAt}
-                  contributors={contributors}
-                  tags={tags}
-                >
-                  {excerpt}
-                </GuidePreview>
-              ),
-            )} */}
-          </Stack>
-        </Container>
-      </Box>
+      <VStack align="left" spacing="0">
+        {allMdx.nodes.map(
+          ({
+            // fields: { createdAt, contributors, slug },
+            frontmatter: { title },
+            // parent: { birthTime },
+            // excerpt,
+            slug
+          }) => (
+            <Link to={slug}>{title}</Link>
+          ),
+        )}
+      </VStack>
     </Layout>
   )
 }
