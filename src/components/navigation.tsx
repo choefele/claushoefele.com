1394193aca64404b1d610c76b51ef63aee6427df
Claus Höfele <claus@claushoefele.com>
2022-12-15 15:59:01 +0100

    Set up navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,9 +1,46 @@
-import {Box} from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  HStack,
+  IconButton,
+  Link,
+  useBreakpointValue,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { FiMenu } from 'react-icons/fi';
 
-export function Navigation(): JSX.Element {
+export default function Navigation(): JSX.Element {
+  const isMenuLarge = useBreakpointValue({ base: false, md: true });
   return (
-    <Box>
-      Navigation
+    <Box as="header" boxShadow="sm">
+      <Box as="nav">
+        <HStack as="ul" justify="space-between">
+          <Button as="li" variant="link" fontSize="xl">
+            <Link as={NextLink} href="/">
+              Claus Höfele
+            </Link>
+          </Button>
+          {isMenuLarge ? (
+            <ButtonGroup variant="ghost" spacing="8">
+              {['Product', 'Pricing', 'Resources', 'Support'].map((item) => (
+                <Button as="li" key={item}>
+                  <Link as={NextLink} href="#">
+                    {item}
+                  </Link>
+                </Button>
+              ))}
+            </ButtonGroup>
+          ) : (
+            <IconButton
+              variant="ghost"
+              icon={<FiMenu fontSize="1.5rem" />}
+              aria-label="Open Menu"
+            />
+          )}
+        </HStack>
+      </Box>
     </Box>
-  )
+  );
 }
