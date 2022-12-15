1068369d72faa3f409e69c9f07916979e8229c4f
Claus Höfele <claus@claushoefele.com>
2022-12-15 16:10:28 +0100

    Extract footer into its own component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
 import Navigation from '../components/navigation';
+import Footer from '../components/footer';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navigation />
       <Component {...pageProps} />
+      <Footer />
     </ChakraProvider>
   );
 }
