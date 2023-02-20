6850281677e41e1f0373bf44d0cc72de881c7c4e
Claus Höfele <claus@claushoefele.com>
2023-02-20 13:04:39 +0100

    Show confirmation after subscription

--- src/components/subscribe-form.tsx
+++ src/components/subscribe-form.tsx
@@ -10,7 +10,7 @@ import {
   InputGroup,
   InputRightElement,
 } from '@chakra-ui/react';
-import { A } from './content';
+import { A, P } from './content';
 import {
   SubscribeRequestData,
   subscribeRequestSchema,
@@ -18,6 +18,7 @@ import {
 } from '../pages/api/subscribe';
 
 export default function SubscribeForm() {
+  const [subscribed, setSubscribed] = useState('');
   const {
     handleSubmit,
     register,
@@ -28,63 +29,70 @@ export default function SubscribeForm() {
   });
   const onSubmit: SubmitHandler<SubscribeRequestData> = async (data) => {
     const result = await subscribeEmail(data);
-    console.log(result);
     if (result.status >= 300) {
       setError('email', {
         type: 'custom',
         message: 'Error subscribing – please try again',
       });
+    } else {
+      setSubscribed(data.email);
     }
   };
 
   // Field doesn't turn red reliably when invalid
   // Show error in proper place - div
-  // Send to API
-  // - useState to remember success; render confirm like https://monicalent.com
-  // - show API error message
   // Decide landing page after opt in
   // Set up SiB emails in english
 
   return (
-    <form noValidate onSubmit={handleSubmit(onSubmit)}>
-      <FormControl
-        isRequired={!subscribeRequestSchema.shape.email.isOptional()}
-        isInvalid={!!errors.email}
-      >
-        <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+    <>
+      {subscribed ? (
+        <P>
+          Thank you for subscribing {subscribed}. Please check your inbox to
+          confirm your email.
+        </P>
+      ) : (
+        <form noValidate onSubmit={handleSubmit(onSubmit)}>
+          <FormControl
+            isRequired={!subscribeRequestSchema.shape.email.isOptional()}
+            isInvalid={!!errors.email}
+          >
+            <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
 
-        <InputGroup size="md">
-          <Input
-            id="email"
-            type="email"
-            placeholder="Your best email"
-            aria-label="Email"
-            pr="7rem"
-            {...register('email')}
+            <InputGroup size="md">
+              <Input
+                id="email"
+                type="email"
+                placeholder="Your best email"
+                aria-label="Email"
+                pr="7rem"
+                {...register('email')}
+              />
+              <InputRightElement width="7rem">
+                <Button
+                  type="submit"
+                  size="sm"
+                  isLoading={isSubmitting}
+                  isDisabled={isSubmitting}
+                >
+                  Subscribe
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <FormHelperText fontSize="xs">
+              We’ll never share your email. Unsubscribe any time.{' '}
+              <A href="https://www.sendinblue.com/legal/privacypolicy/">
+                Privacy policy.
+              </A>
+            </FormHelperText>
+          </FormControl>
+          <input
+            type="hidden"
+            value={process.env.NEXT_PUBLIC_SUBSCRIBE_ID}
+            {...register('subscribeId')}
           />
-          <InputRightElement width="7rem">
-            <Button
-              type="submit"
-              size="sm"
-              isLoading={isSubmitting}
-              isDisabled={isSubmitting}
-            >
-              Subscribe
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-        <FormHelperText fontSize="xs">
-          We’ll never share your email. Unsubscribe any time.{' '}
-          <A href="https://www.sendinblue.com/legal/privacypolicy/">
-            Privacy policy.
-          </A>
-        </FormHelperText>
-      </FormControl>
-      <input
-        type="hidden"
-        value={process.env.NEXT_PUBLIC_SUBSCRIBE_ID}
-        {...register('subscribeId')}
-      />
-    </form>
+        </form>
+      )}
+    </>
   );
 }
