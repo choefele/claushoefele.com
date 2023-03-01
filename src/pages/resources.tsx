4ce61615a444802bfc729b9fd689537d9b099912
Claus Höfele <claus@claushoefele.com>
2023-03-01 18:23:43 +0100

    Add resources page

---
+++ src/pages/resources.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import { H1, H2, P } from '../components/content';
+
+export default function Resources(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Claus Höfele - Resources</title>
+      </Head>
+
+      <main>
+        <H1>Resources</H1>
+      </main>
+    </>
+  );
+}
