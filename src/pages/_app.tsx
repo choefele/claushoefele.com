6d1ab567871e4ab5349b4d0b89eb5e1447c30cd2
Claus Höfele <claus@claushoefele.com>
2023-03-14 18:36:20 +0100

    Remove favicon for now

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,9 +8,7 @@ import Theme from '../theme';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head>{/* <link rel="icon" href="/favicon.ico" /> */}</Head>
       <ChakraProvider theme={Theme}>
         <Container maxW="5xl">
           <Header />
