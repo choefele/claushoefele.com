ec4fc808e03c79dcf1d9140d0ccac0917ff9ede0
Claus Höfele <claus@claushoefele.com>
2023-03-29 10:59:19 +0200

    Increase margin in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 import { Text, Box, Divider } from '@chakra-ui/react';
 
 export default function Footer(): JSX.Element {
-  return <Box as="footer"></Box>;
+  return <Box as="footer" my="4rem"></Box>;
 }
